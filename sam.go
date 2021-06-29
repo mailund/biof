38d040f8866d2d1329bf65b59fc0ae6ac7ebd53e
Thomas Mailund <mailund@birc.au.dk>
2021-06-29 13:17:25 +0200

    newline in sam

--- sam.go
+++ sam.go
@@ -6,6 +6,6 @@ import (
 )
 
 func PrintSam(w io.Writer, qname, rname string, pos int, cigar, read, qual string) error {
-	_, err := fmt.Fprintf(w, "%s\t0\t%s\t%d\t0\t%s\t*\t0\t0\t%s\t%s", qname, rname, pos+1, cigar, read, qual)
+	_, err := fmt.Fprintf(w, "%s\t0\t%s\t%d\t0\t%s\t*\t0\t0\t%s\t%s\n", qname, rname, pos+1, cigar, read, qual)
 	return err
 }
